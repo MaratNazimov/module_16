c286d096cda644c498dc357d2096ec33557fb3e9
MaratNazimov <marat.abdurahimov.88@yandex.ru>
2024-11-30 16:27:50 +0700

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -3,17 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def main_page():
-    return "Главная страница"
+async def main_page() -> dict:
+    return {"message": "Главная страница"}
 
 @app.get("/user/admin")
-async def user_admin():
-    return "Вы вошли как администратор"
+async def user_admin() -> dict:
+    return {"message": "Вы вошли как администратор"}
 
 @app.get("/user/{user_id}")
-async def users_id(user_id = int):
-    return f"Вы вошли как пользователь № {user_id}"
+async def users_id(user_id: int) -> dict:
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
 
 @app.get("/user/{username}/{age}")
-async def users(username = str, age = int):
-    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
+async def users(username: str, age: int) -> dict:
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
